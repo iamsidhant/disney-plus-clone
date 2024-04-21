5a5b022211559cfcbae317b9b233150682b4fdff
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-04-21 21:20:44 +0530

    added and styled background image and logos

--- disney-clone/src/components/Login.js
+++ disney-clone/src/components/Login.js
@@ -4,7 +4,12 @@ import styled from "styled-components";
 const Login = (props) => {
   return (
     <Container>
-        <Content>Content</Content>
+        <Content>
+          <CTA>
+            <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          </CTA>
+          <BgImage />
+        </Content>
     </Container>
   )
 }
@@ -29,6 +34,34 @@ const Content = styled.section`
     flex-direction: column;
     padding: 80px 40px;
     height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-width: 1px;
+  display: block;
+  width: 100%;
 `;
 
 export default Login;

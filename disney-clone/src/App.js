31cf91b5b5a965a48c54c7568b6225e53e9f911d
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-04-22 11:17:48 +0530

    added Header component

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>

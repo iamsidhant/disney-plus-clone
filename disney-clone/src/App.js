0656307a45bb74665c411b858b863c47f741dd74
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-04-25 12:49:59 +0530

    Login functionality (error)

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -1,54 +1,93 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { auth, provider } from '../firebase'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { 
+    selectUserName,
+    selectUserPhoto, 
+    setUserLoginDetails 
+} from '../features/user/userSlice';
 
 const Header = (props) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userName = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                setUser(user);
+                navigate("/home");
+            }
+        })
+    }, [userName]);
+
     const handleAuth = () => {
         auth
             .signInWithPopup(provider)
             .then((result) => {
-                console.log(result);
+                setUser(result.user);
             })
             .catch((error) => {
                 alert(error.message);
             });
     }
+
+    const setUser = (user) => {
+        dispatch(
+            setUserLoginDetails({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL,
+            })
+        );
+    };
+
   return (
     <Nav>
         <Logo>
             <img src='/images/logo.svg' alt='Disney+' />
         </Logo>
 
-        <NavMenu>
-            <a href="/home">
-                <img src='/images/home-icon.svg' alt='HOME' />
-                <span>HOME</span>
-            </a>
-            <a>
-                <img src='/images/search-icon.svg' alt='SEARCH' />
-                <span>SEARCH</span>
-            </a>
-            <a>
-                <img src='/images/watchlist-icon.svg' alt='WATCHLIST' />
-                <span>WATCHLIST</span>
-            </a>
-            <a>
-                <img src='/images/original-icon.svg' alt='ORIGINALS' />
-                <span>ORIGINALS</span>
-            </a>
-            <a>
-                <img src='/images/movie-icon.svg' alt='MOVIES' />
-                <span>MOVIES</span>
-            </a>
-            <a>
-                <img src='/images/series-icon.svg' alt='SERIES' />
-                <span>SERIES</span>
-            </a>
-        </NavMenu>
-        <Login onClick={handleAuth}>Login</Login>
+        {!userName ? (
+                <Login onClick={handleAuth}>Login</Login>
+            ) : (
+                <>
+                <NavMenu>
+                    <a href="/home">
+                        <img src='/images/home-icon.svg' alt='HOME' />
+                        <span>HOME</span>
+                    </a>
+                    <a>
+                        <img src='/images/search-icon.svg' alt='SEARCH' />
+                        <span>SEARCH</span>
+                    </a>
+                    <a>
+                        <img src='/images/watchlist-icon.svg' alt='WATCHLIST' />
+                        <span>WATCHLIST</span>
+                    </a>
+                    <a>
+                        <img src='/images/original-icon.svg' alt='ORIGINALS' />
+                        <span>ORIGINALS</span>
+                    </a>
+                    <a>
+                        <img src='/images/movie-icon.svg' alt='MOVIES' />
+                        <span>MOVIES</span>
+                    </a>
+                    <a>
+                        <img src='/images/series-icon.svg' alt='SERIES' />
+                        <span>SERIES</span>
+                    </a>
+                </NavMenu>
+                <UserImg src={userPhoto} alt={userName} />
+                </>
+            )
+        }
     </Nav>
-  )
-}
+  );
+};
 
 const Nav = styled.nav`
     position: fixed;
@@ -160,6 +199,10 @@ const Login = styled.a`
         color: #000;
         border-color: transparent;
     }
+`;
+
+const UserImg = styled.img`
+    height: 100%;
 `;
 
 export default Header;
